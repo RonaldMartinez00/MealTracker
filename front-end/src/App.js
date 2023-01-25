e9c056b3aa6c0fe5631cfd1f1a3999ccd6109fe4
melissarubiio <melirubio0603@gmail.com>
2023-01-25 12:51:08 -0700

    making api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,8 @@ import Login from './views/login.js';
 import CurrentUserProvider from './context/currentuser';
 import Signup from './views/signup';
 import Home from './views/home';
-import Navbar from './views/navbar'
+import Navbar from './views/navbar';
+import Api from './views/api';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path='/signup' element={<Signup/>}/>
         <Route exact path='/login' element={<Login/>}/>
         <Route exact path='/navbar' element={<Navbar/>}/>
+        <Route exact path='/api' element={<Api/>}/>
       </Routes>
     </CurrentUserProvider>
   );
