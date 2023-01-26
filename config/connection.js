8546736ac40acfaac802957f6683779bac7589da
RonaldMartinez00 <mronald16@yahoo.com>
2023-01-26 14:41:12 -0800

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ await mongoose.connect(uri,
     });
 };
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/mealtrackerDB',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
