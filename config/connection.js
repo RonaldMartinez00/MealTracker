ff19e6490b4dc304ebc177cc269d8dd45fecedc2
RonaldMartinez00 <mronald16@yahoo.com>
2023-01-26 14:38:09 -0800

    connected to heroku

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ async function main(){
 await mongoose.connect(uri, 
 { useNewUrlParser: true, useUnifiedTopology: true,
     });
-}
+};
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
+
+
+
+
 
 module.exports = main;
